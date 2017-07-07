9ddba01f471007c41b27239c3379c1e4f75b9626
francisleunggie <francisleunggie@gmail.com>
2017-07-08 00:54:06 +0800

    Francis: test deploy 4

--- demos/web/websocket-server.py
+++ demos/web/websocket-server.py
@@ -294,7 +294,7 @@ class OpenFaceServerProtocol(WebSocketServerProtocol):
 					identity = len(identities)
 					identities.append(identity)
 					self.images[phash] = Face(rep, identity)
-					# self.people.append(str(time.time()))
+					self.people.append(str(time.time()))
 					# TODO: Transferring as a string is suboptimal.
 					# content = [str(x) for x in cv2.resize(alignedFace, (0,0),
 					# fx=0.5, fy=0.5).flatten()]
