0a03893c35614af8ad41c032422bebe96ef6a4a1
francisleunggie <francisleunggie@gmail.com>
2017-07-09 18:30:29 +0800

    Francis: add person 29

--- demos/web/websocket-server.py
+++ demos/web/websocket-server.py
@@ -267,7 +267,7 @@ class OpenFaceServerProtocol(WebSocketServerProtocol):
 			rep1 = face.rep
 			diff = rep - rep1
 			diff = np.dot(diff, diff)
-			if comparison[face.identity] is None:
+			if face.identity in comparison:
 				comparison[face.identity] = { "diff": diff, "denom": 1}
 			else: 
 				comparison[face.identity].diff += diff
